0c2310ea8324aba9763377a6393f76b5edfcef8d
alaaalshaibi <alaa.alshebe@gmail.com>
2023-08-14 15:17:13 +0300

    ui home

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,12 @@
-import 'package:e_commerce_app/controller/home_controller.dart';
 import 'package:e_commerce_app/core/constant/color.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    HomeControllerImp controllerImp = Get.put(HomeControllerImp());
+    //HomeControllerImp controllerImp = Get.put(HomeControllerImp());
     return Scaffold(
       body: ListView(
         children: [
